921f632e8bbcf263427ac55b1267b71c837e1930
Timothy G <timg51237@gmail.com>
2024-01-27 17:59:13 -0800

    cleanup

--- server/app/routers/data.py
+++ server/app/routers/data.py
@@ -1,16 +1,11 @@
-from fastapi import APIRouter, HTTPException, Query, Depends
+from fastapi import APIRouter
 import together
 import pymongo
-
-
-import pymongo
-from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
-from llama_index.storage.storage_context import StorageContext
-from llama_index.readers.file.base import SimpleDirectoryReader
-from llama_index import SimpleDirectoryReader, ServiceContext, VectorStoreIndex
-from llama_index.embeddings import TogetherEmbedding
-from llama_index.llms import TogetherLLM
 from typing import List
+
+router = APIRouter()
+router = APIRouter(prefix="/data", tags=["data"])
+
 
 
 # Provide a template following the LLM's original chat template.
@@ -39,19 +34,15 @@ def generate_embeddings(input_texts: List[str], model_api_string: str) -> List[L
 
 
 
-router = APIRouter()
-router = APIRouter(prefix="/data", tags=["data"])
-
 
 @router.post("/resume_add", )
-async def try_add_resume_data():
+async def try_add_resume_data(text: str):
 
     embedding_model_string = 'togethercomputer/m2-bert-80M-8k-retrieval' # model API string from Together.
     vector_database_field_name = 'embedding_together_m2-bert-8k-retrieval' # define your embedding field name.
     NUM_DOC_LIMIT = 200 # the number of documents you will process and generate embeddings.
 
-    vector_embedding = generate_embeddings(["This is a test."], embedding_model_string)
-    print(vector_embedding)
+    vector_embedding = generate_embeddings([text], embedding_model_string)
 
     # documents = SimpleDirectoryReader(
     #     input_files=["/Users/timg/Documents/GitHub/mongo-hackathon/server/TIMOTHY_GUO_RESUME.pdf"]
@@ -65,20 +56,6 @@ async def try_add_resume_data():
     mongodb_db = mongodb_client["beta"]
     mongodb_resumes = mongodb_db["resumes"]
     mongodb_resumes.insert_one({ vector_database_field_name: vector_embedding})
-
-
-    # store = MongoDBAtlasVectorSearch(mongodb_client)
-    # storage_context = StorageContext.from_defaults(vector_store=store)
-
-    # print("Got storage context")
-    
-    # index = VectorStoreIndex.from_documents(
-    #     uber_docs, storage_context=storage_context
-    # )
-
-    # print("Loaded into mongo")
-
-    # response = index.as_query_engine().query("What is this document?")
 
     return { "response": "response"}
     
