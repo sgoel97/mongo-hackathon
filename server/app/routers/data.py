63d76015a8909309f99939f63529d6433d386ee7
Timothy G <timg51237@gmail.com>
2024-01-27 19:28:56 -0800

    fixed chat

--- server/app/routers/data.py
+++ server/app/routers/data.py
@@ -114,6 +114,9 @@ def get_resume_to_text(
 
 @router.post("/query")
 async def try_query_resume_data(query: str):
+    return query_resume_data(query)
+
+def query_resume_data(query: str):
     embedding_model_string = (
         "togethercomputer/m2-bert-80M-8k-retrieval"  # model API string from Together.
     )
@@ -172,5 +175,36 @@ async def try_add_resume_data(text: str):
     mongodb_resumes.insert_one(
         {vector_database_field_name: vector_embedding[0], "text": text}
     )
+
+    return {"response": "response"}
+
+
+
+def add_batch_resumes(resumes: List[dict]):
+
+    embedding_model_string = (
+        "togethercomputer/m2-bert-80M-8k-retrieval"  # model API string from Together.
+    )
+    vector_database_field_name = (
+        "embedding_together_m2-bert-8k-retrieval"  # define your embedding field name.
+    )
+    NUM_DOC_LIMIT = (
+        200  # the number of documents you will process and generate embeddings.
+    )
+
+    for resume in resumes:
+        vector_embedding = generate_embeddings([text], embedding_model_string)
+
+        # documents = SimpleDirectoryReader(
+        #     input_files=["/Users/timg/Documents/GitHub/mongo-hackathon/server/TIMOTHY_GUO_RESUME.pdf"]
+        # ).load_data()
+
+        mongo_uri = "mongodb+srv://timg51237:01Y4sSZbZxsNFydW@cluster0.qbsk5ke.mongodb.net/?retryWrites=true&w=majority"
+        mongodb_client = pymongo.MongoClient(mongo_uri)
+        mongodb_db = mongodb_client["beta"]
+        mongodb_resumes = mongodb_db["resumes"]
+        mongodb_resumes.insert_one(
+            {vector_database_field_name: vector_embedding[0], "text": text}
+        )
 
     return {"response": "response"}
