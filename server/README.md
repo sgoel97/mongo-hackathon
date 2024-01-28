e7a7450fc007deef58af521dbbc5777d014be162
Timothy G <timg51237@gmail.com>
2024-01-27 17:24:02 -0800

    some mongo stuff?

--- server/README.md
+++ server/README.md
@@ -1,6 +1,6 @@
 ```bash
-pip install "fastapi[all]" llama-index pymongo
+pip install "fastapi[all]" llama-index pymongo pypdf
 
-uvicorn app.main:app --reload
+OPENAI_API_KEY=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 uvicorn app.main:app --reload
 
 ```
--- server/app/routers/data.py
+++ server/app/routers/data.py
@@ -19,9 +19,10 @@ from llama_index.storage.storage_context import StorageContext
 from llama_index.readers.file.base import SimpleDirectoryReader
 
 router = APIRouter()
+router = APIRouter(prefix="/data", tags=["data"])
 
 
-@router.post("/data/resume_try", )
+@router.post("/resume_try", )
 async def try_read_resume_data():
     mongo_uri = (
         "mongodb+srv://timg51237:01Y4sSZbZxsNFydW@cluster0.qbsk5ke.mongodb.net/?retryWrites=true&w=majority"
@@ -30,12 +31,18 @@ async def try_read_resume_data():
     mongodb_client = pymongo.MongoClient(mongo_uri)
     store = MongoDBAtlasVectorSearch(mongodb_client)
     storage_context = StorageContext.from_defaults(vector_store=store)
+
+    print("Got storage context")
     uber_docs = SimpleDirectoryReader(
-        input_files=["./data/10k/uber_2021.pdf"]
+        input_files=["/Users/timg/Documents/GitHub/mongo-hackathon/server/TIMOTHY_GUO_RESUME.pdf"]
     ).load_data()
     index = VectorStoreIndex.from_documents(
         uber_docs, storage_context=storage_context
     )
 
-    return {}
+    print("Loaded into mongo")
+
+    response = index.as_query_engine().query("What is this document?")
+
+    return { response: "response"}
     
